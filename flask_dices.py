2883549c7fe54b0a031169f1e5f66bad5d8de753
Sofia Denner <sofi.denner@gmail.com>
2018-02-13 15:06:49 -0300

    home template response

--- flask_dices.py
+++ flask_dices.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'Welcome to Dark Souls board game dice statistics'
+    return render_template('home.html')
